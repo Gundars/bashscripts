2c3c620e9b014b2bd72950d83a1e85928a443d1f
Gundars Mēness <gundars.meness@accenture.com>
2016-03-18 17:01:37 +0200

    Update gitcount.sh

--- scripts/gitcount.sh
+++ scripts/gitcount.sh
@@ -3,13 +3,13 @@
 apiToken=77885...........
 apiRoot=https://api.github.com
 owner=discovery-fusion
-repos=(fusion-api-user fusion-lib-payment fusion-subscription fusion-account-management fusion-authorisation fusion-authentication fu$
+repos=(fusion-api-user fusion-lib-payment fusion-subscription fusion-account-management fusion-authorisation fusion-authentication fusion-session-management fusion-commons-core fusion-build-utilities fusion-lib-vouchers fusion-platform-performance-xhprof fusion-lib-wireless-factory)
 count=0
 delimiter='  '
 for repo in ${repos[@]}; do
-        pullJson=$(curl -s $apiRoot/repos/$owner/$repo/pulls?access_token=$apiToken&state=open&base=master)
-        pullCount=$(echo ${pullJson} | jq '. | length')
-        printf "%s %s $repo \n" $pullCount "${delimiter:${#pullCount}}"
-        count=`expr $count + $pullCount`
+	pullJson=$(curl -s $apiRoot/repos/$owner/$repo/pulls?access_token=$apiToken&state=open&base=master)
+	pullCount=$(echo ${pullJson} | jq '. | length')
+	printf "%s %s $repo \n" $pullCount "${delimiter:${#pullCount}}"
+	count=`expr $count + $pullCount`
 done
 echo TOTAL PRs: $count
