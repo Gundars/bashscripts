337d51824bf2c0e4a62b5c4f0da15160cb65d314
gundars <me@gundars.me>
2016-03-25 14:41:43 +0200

    release-user-api.php selection part done

--- scripts/release-user-api.php
+++ scripts/release-user-api.php
@@ -1,16 +1,24 @@
 <?php
 
+namespace Releaser;
+
 class Releaser
 {
     # generate your Github Token @ https://github.com/settings/tokens/new  with "repo" access
-    const GITHUB_TOKEN = '';
+    const GITHUB_TOKEN = '2e34287ca20fe380aa8fc54b6eea270140a48e84';
     const GITHUB_OWNER = 'discovery-fusion';
     const GITHUB_ROOT  = 'https://api.github.com/';
 
-    private $repoLatestMasterVersions;
+    private $versionsToBeReleased = [];
     private $currentRepo;
-
+    private $currentMasterReleaseVersion;
+    private $currentPatchReleaseVersion;
+    private $nextMasterReleaseVersion;
+    private $nextPatchReleaseVersion;
     // todo:
+    // combinate dependency tree to f.eks. release new User API if deps needs a release
+    // release with changed dep versions
+
     // options:
     // master cut
     // patch master
@@ -19,42 +27,69 @@ class Releaser
     public function __construct()
     {
         // common repos
-        $this->releaseIfNewCommits('fusion-build-utilities');
-        $this->releaseIfNewCommits('fusion-commons-core');
+        $this->verifyStatus('fusion-build-utilities');
+        $this->verifyStatus('fusion-commons-core');
 
         // packages
-        $this->releaseIfNewCommits('fusion-lib-wireless-factory');
-        $this->releaseIfNewCommits('fusion-lib-vouchers');
-        $this->releaseIfNewCommits('fusion-session-management');
-        $this->releaseIfNewCommits('fusion-authentication');
-        $this->releaseIfNewCommits('fusion-authorisation');
-        $this->releaseIfNewCommits('fusion-account-management');
-        $this->releaseIfNewCommits('fusion-subscription');
-        $this->releaseIfNewCommits('fusion-lib-payment');
-        $this->releaseIfNewCommits('fusion-lib-payment');
+        $this->verifyStatus('fusion-lib-wireless-factory');
+//        $this->verifyStatus('fusion-lib-vouchers');
+//        $this->verifyStatus('fusion-session-management');
+//        $this->verifyStatus('fusion-authentication');
+//        $this->verifyStatus('fusion-authorisation');
+//        $this->verifyStatus('fusion-account-management');
+//        $this->verifyStatus('fusion-subscription');
+//        $this->verifyStatus('fusion-lib-payment');
+//        $this->verifyStatus('fusion-lib-payment');
 
         // main user API
-        $this->releaseIfNewCommits('fusion-api-user');
+        $this->verifyStatus('fusion-api-user');
+
+        $this->release();
     }
 
-    private function releaseIfNewCommits($repo)
+    private function release()
+    {
+        $count = count($this->versionsToBeReleased);
+        $this->msg("\n");
+        if ($count === 0) {
+            $this->msg("No repositories require a release! :)");
+            die;
+        }
+
+        $this->msg("$count repositories to be released:");
+        foreach ($this->versionsToBeReleased as $repo => $version) {
+            $this->msg("$version $repo");
+        }
+
+        $this->promptUserWhetherToProceed();
+
+
+
+
+    }
+
+    private function promptUserWhetherToProceed()
+    {
+        $this->msg("\n");
+        $this->msg("Are you sure you want to release these packages?");
+        $this->msg("Type YES(y) to continue, NO(n) to abort...");
+        $handle = fopen ("php://stdin","r");
+        $line = fgets($handle);
+        $userVal = trim($line);
+        if(!in_array($userVal, ['y', 'Y', 'yes', 'YES'])){
+            $this->msg("\nAborting!");
+            exit;
+        }
+        fclose($handle);
+        $this->msg("\nContinuing with release, press  Ctrl+C  to abort manually");
+    }
+
+    private function verifyStatus($repo)
     {
         $this->currentRepo = $repo;
         $releases          = $this->curlGetReleases();
-        list($lastMasterV, $lastPatchV) = $this->getLatestVersions($releases);
-        $this->log(
-            "{$this->currentRepo} latest master release $lastMasterV, currently patched: "
-            . ($lastPatchV ? $lastPatchV : 'no')
-        );
-        $needsRelease = $this->branchNeedsANewRelease('master', $lastMasterV);
-
-        if ($needsRelease) {
-            $this->log("$this->currentRepo needs new master release!");
-
-            $this->repoLatestMasterVersions[$this->currentRepo] = 'some newly created repo master cut version';
-        } else {
-            $this->repoLatestMasterVersions[$this->currentRepo] = $lastMasterV;
-        }
+        $this->getLatestVersions($releases);
+        $this->branchNeedsANewRelease('master', $this->currentMasterReleaseVersion);
     }
 
     private function getLatestVersions($releases)
@@ -79,22 +114,31 @@ class Releaser
         $masterVMaxLevel1 = max(array_keys($tagsThreeLevels));
         $masterVMaxLevel2 = max(array_keys($tagsThreeLevels[$masterVMaxLevel1]));
         $masterVMaxLevel3 = max($tagsThreeLevels[$masterVMaxLevel1][$masterVMaxLevel2]);
-        $masterVMax       = "$masterVMaxLevel1.$masterVMaxLevel2.$masterVMaxLevel3";
+        $this->currentMasterReleaseVersion = "$masterVMaxLevel1.$masterVMaxLevel2.$masterVMaxLevel3";
+        $this->nextMasterReleaseVersion = "$masterVMaxLevel1." . ($masterVMaxLevel2 + 1) . ".$masterVMaxLevel3";
 
         $patchVMaxLevel3 = min($tagsThreeLevels[$masterVMaxLevel1][$masterVMaxLevel2]);
-        $patchVMax       = ($patchVMaxLevel3 > 0) ? "$masterVMaxLevel1.$masterVMaxLevel2.$patchVMaxLevel3" : null;
+        $this->currentPatchReleaseVersion = ($patchVMaxLevel3 > 0) ? "$masterVMaxLevel1.$masterVMaxLevel2.$patchVMaxLevel3" : null;
+        $this->nextPatchReleaseVersion = ($this->currentPatchReleaseVersion) ? "$masterVMaxLevel1.$masterVMaxLevel2." . ($patchVMaxLevel3 + 1) : null;
 
-        return [$masterVMax, $patchVMax];
+        $this->msg(
+            "{$this->currentRepo} latest master release $this->currentMasterReleaseVersion, "
+            . ($this->currentPatchReleaseVersion ? 'patched with ' . $this->currentPatchReleaseVersion  : 'not patched')
+        );
     }
 
     private function branchNeedsANewRelease($branch, $releaseVersion)
     {
         $comparison = $this->curlMasterReleaseAndMasterComparison($branch, $releaseVersion);
-        $this->log(
-            "$this->currentRepo master branch is {$comparison->status} comparing to master release. Ahead by {$comparison->ahead_by}, behind by {$comparison->behind_by} commits"
+        $this->msg(
+            "$this->currentRepo $branch branch is {$comparison->status} comparing to master release. Ahead by {$comparison->ahead_by}, behind by {$comparison->behind_by} commits"
         );
 
-        return ($comparison->ahead_by > 0);
+        $needsRelease = ($comparison->ahead_by > 0);
+        if ($needsRelease) {
+            $this->versionsToBeReleased[$this->currentRepo] = $this->nextMasterReleaseVersion;
+            $this->msg("> $this->currentRepo needs new release ");
+        }
     }
 
     private function curlMasterReleaseAndMasterComparison($branch, $releaseVersion)
@@ -115,7 +159,14 @@ class Releaser
     {
         $path = 'repos/' . static::GITHUB_OWNER . '/' . $this->currentRepo . '/releases';
 
-        return @json_decode($this->executeCurlRequest($path));
+        $releases = @json_decode($this->executeCurlRequest($path));
+        if (isset($releases->message)) {
+            $t = (static::GITHUB_TOKEN == '') ? ' const GITHUB_TOKEN is empty!' : '';
+            $this->msg("{$releases->message}. $t ABORTING");
+            exit;
+        }
+
+        return $releases;
     }
 
     private function executeCurlRequest($urlPath)
@@ -134,7 +185,7 @@ class Releaser
         return curl_exec($ch);
     }
 
-    private function log($message)
+    private function msg($message)
     {
         echo "$message \n";
     }
